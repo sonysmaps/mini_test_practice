09bda7d1c49b21977f001371013d3fbb6b10c712
Tatsuhito Yamada <sonysmaps@gmail.com>
2014-11-25 10:36:45 +0000

    4th commit

--- lib/mini_test_practice.rb
+++ lib/mini_test_practice.rb
@@ -13,7 +13,8 @@ module MiniTestPractice
     end
 
     def check_number?(value)
-      if value.between?(1:9999) and value.even? 
+#      if  value.between?(1:9999) and value.even? 
+      if    (value >= 1) and (value <= 9999) and (value % 2) == 0
         return true
       else
         return false
